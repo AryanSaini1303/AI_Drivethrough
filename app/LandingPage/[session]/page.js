8b980145cbf116d008a9dbfe0c890045e1828610
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-08-11 15:00:53 +0530

    Feat: protected landing page route

--- app/LandingPage/[session]/page.js
+++ app/LandingPage/[session]/page.js
@@ -1,23 +1,37 @@
 "use client";
 import { useEffect, useState } from "react";
 import style from "./page.module.css";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 export default function LandingPage({ params }) {
   const router = useRouter();
   const [userData, setUserData] = useState({});
+  const { data: session, status } = useSession();
+  console.log(status);
   useEffect(() => {
     setUserData(JSON.parse(decodeURIComponent(params.session)).user);
-  }, []);
+    setTimeout(() => {
+      if (status === "unauthenticated") {
+        signOut({callbackUrl:"/"}).then(()=>{
+          router.push("/");
+        })
+      }
+    }, 1000);
+  }, [status]);
+  if (status === "unauthenticated") {
+    return "Unauthenticated";
+  }
   return (
-    <div className={style.wrapper}>
-      <button
-        onClick={() => {
-          signOut({ callbackUrl: "/" }).then(() => router.push("/"));
-        }}
-      >
-        Sign Out
-      </button>
-    </div>
+    status != "loading" && (
+      <div className={style.wrapper}>
+        <button
+          onClick={() => {
+            signOut({ callbackUrl: "/" }).then(() => router.push("/"));
+          }}
+        >
+          Sign Out
+        </button>
+      </div>
+    )
   );
 }
