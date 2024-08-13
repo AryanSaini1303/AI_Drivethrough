2d4b987efa5bfc31f5794173f863577a26611afd
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-08-13 12:24:57 +0530

    save

--- app/LandingPage/[session]/page.js
+++ app/LandingPage/[session]/page.js
@@ -3,18 +3,37 @@ import { useEffect, useState } from "react";
 import style from "./page.module.css";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
 export default function LandingPage({ params }) {
   const router = useRouter();
   const [userData, setUserData] = useState({});
   const { data: session, status } = useSession();
-  console.log(status);
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY);
+  const [centerlat, setCenterLat] = useState();
+  const [centerlng, setCenterLng] = useState();
+  const center = {
+    lat: parseFloat(`${centerlat}`),
+    lng: parseFloat(`${centerlng}`),
+  };
+  const geo = navigator.geolocation;
+  geo.getCurrentPosition(getCoords);
+  function getCoords(position) {
+    setCenterLat(position.coords.latitude);
+    setCenterLng(position.coords.longitude);
+  }
+  console.log(centerlat);
+  console.log(centerlng);
+  console.log(center);
   useEffect(() => {
     setUserData(JSON.parse(decodeURIComponent(params.session)).user);
     setTimeout(() => {
       if (status === "unauthenticated") {
-        signOut({callbackUrl:"/"}).then(()=>{
+        signOut({ callbackUrl: "/" }).then(() => {
           router.push("/");
-        })
+        });
       }
     }, 1000);
   }, [status]);
@@ -22,7 +41,8 @@ export default function LandingPage({ params }) {
     return "Unauthenticated";
   }
   return (
-    status != "loading" && (
+    status != "loading" &&
+    isLoaded && (
       <div className={style.wrapper}>
         <button
           onClick={() => {
@@ -31,6 +51,11 @@ export default function LandingPage({ params }) {
         >
           Sign Out
         </button>
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100vw", height: "100vh" }}
+        ></GoogleMap>
       </div>
     )
   );
