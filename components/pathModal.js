e703ac50e7beb2d4805f1bd14f084e322db94f07
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-08-23 23:37:10 +0530

    Feat: Dark mode map added with working pathModal

---
+++ components/pathModal.js
@@ -0,0 +1,85 @@
+"use client";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import styles from "./pathModal.module.css";
+import { Autocomplete } from "@react-google-maps/api";
+import { useEffect, useRef, useState } from "react";
+
+export default function PathModal({ map, center, getDirectionsResponse }) {
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+  const originRef = useRef();
+  const destinationRef = useRef();
+  useEffect(() => {
+    getDirectionsResponse(directionsResponse);
+  }, [directionsResponse]);
+  async function calculateRoute(e) {
+    e.preventDefault();
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
+  function clearRoute() {
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destinationRef.current.value = "";
+  }
+  return (
+    <div className={styles.container}>
+      <section className={styles.inputSection}>
+        <form onSubmit={calculateRoute}>
+          <Autocomplete>
+            <input
+              type="text"
+              name="Origin"
+              id=""
+              placeholder="Origin"
+              ref={originRef}
+            />
+          </Autocomplete>
+          <Autocomplete>
+            <input
+              type="text"
+              name="Destination"
+              id=""
+              placeholder="Destination"
+              ref={destinationRef}
+            />
+          </Autocomplete>
+          <button type="submit">Calculate Route</button>
+          <button type="button" onClick={clearRoute}>
+            clear
+          </button>
+        </form>
+      </section>
+      <section className={styles.infoSection}>
+        <h5>
+          Distance: <span>{distance}</span>
+        </h5>
+        <h5>
+          Duration: <span>{duration}</span>
+        </h5>
+        <button
+          type="button"
+          onClick={() => {
+            map.panTo(center);
+          }}
+        >
+          center
+        </button>
+      </section>
+    </div>
+  );
+}
