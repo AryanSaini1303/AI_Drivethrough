c49ddb8498446996f8c7c6017aeb3c3d03276b5f
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-08-30 18:11:14 +0530

    save

--- components/speedDials.js
+++ components/speedDials.js
@@ -1,22 +1,34 @@
-'use client'
-import { useEffect, useState } from "react"
-import styles from "./speedDials.module.css"
-export default function SpeedDials({speed, predictedSpeed, trafficSignalSaturation, reachingProbability}){
-    // const [prevSpeed,setPrevSpeed]=useState(null);
-    // useEffect(()=>{
-    //     console.log(prevSpeed-predictedSpeed);
-    //     setPrevSpeed(predictedSpeed);
-    // },[predictedSpeed])
-    const [predictedSpeedNext,setPredictedSpeedNext]=useState(0);
-    setInterval(() => {
-        setPredictedSpeedNext(predictedSpeed);
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./speedDials.module.css";
+
+export default function SpeedDials({
+  speed,
+  predictedSpeed,
+  trafficSignalSaturation,
+  reachingProbability,
+}) {
+  const [predictedSpeedNext, setPredictedSpeedNext] = useState(predictedSpeed);
+  useEffect(() => {
+    // Set a timeout to update predictedSpeedNext after 2 seconds
+    const timer = setTimeout(() => {
+      setPredictedSpeedNext(predictedSpeed);
     }, 2000);
-    console.log(reachingProbability);
-    return(
-        <div className={styles.container}>
-            <div className={styles.probCurve} style={reachingProbability===25?{transform:"rotate(-180deg)"}:null}></div>
-            <h1 className={styles.speed}>{trafficSignalSaturation?"Max":Math.floor(predictedSpeedNext*3.6)}</h1>
-            <h3>Kmph</h3>
-        </div>
-    )
+    // Cleanup the timeout if predictedSpeed changes before the timeout completes
+    return () => clearTimeout(timer);
+  }, [predictedSpeed]);
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.probCurve}
+        style={
+          reachingProbability === 25 ? { transform: "rotate(-180deg)" } : null
+        }
+      ></div>
+      <h1 className={styles.speed}>
+        {trafficSignalSaturation ? "Max" : Math.floor(predictedSpeedNext * 3.6)}
+      </h1>
+      <h3>Kmph</h3>
+    </div>
+  );
 }
