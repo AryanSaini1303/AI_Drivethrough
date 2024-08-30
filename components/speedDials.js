cc51fd434384818294b189834d8628f9dfb349f2
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-08-30 18:02:27 +0530

    Testing

--- components/speedDials.js
+++ components/speedDials.js
@@ -7,11 +7,15 @@ export default function SpeedDials({speed, predictedSpeed, trafficSignalSaturati
     //     console.log(prevSpeed-predictedSpeed);
     //     setPrevSpeed(predictedSpeed);
     // },[predictedSpeed])
+    const [predictedSpeedNext,setPredictedSpeedNext]=useState(0);
+    setInterval(() => {
+        setPredictedSpeedNext(predictedSpeed);
+    }, 1000);
     console.log(reachingProbability);
     return(
         <div className={styles.container}>
             <div className={styles.probCurve} style={reachingProbability===25?{transform:"rotate(-180deg)"}:null}></div>
-            <h1 className={styles.speed}>{trafficSignalSaturation?"Max":Math.floor(predictedSpeed*3.6)}</h1>
+            <h1 className={styles.speed}>{trafficSignalSaturation?"Max":Math.floor(predictedSpeedNext*3.6)}</h1>
             <h3>Kmph</h3>
         </div>
     )
